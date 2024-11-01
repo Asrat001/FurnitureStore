db676642c6e4109bb15374a51df97aaedc6867e4
assrat007 <asratadane169@gmail.com>
2024-11-01 22:21:47 +0300

    fixing eslint

--- components/ProductSection.tsx
+++ components/ProductSection.tsx
@@ -1,16 +1,16 @@
-'use client'
+"use client";
 
-import Modal from '@/components/Modal'
-import { ProductImageList } from '@/lib/utils'
-import Image from 'next/image'
-import React, { useState } from 'react'
+
+import { ProductImageList } from "@/lib/utils";
+import Image from "next/image";
+import React, { useState } from "react";
+import Modal from "./Modal";
 
 export const ProductSection = () => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [selectedImage, setSelectedImage] = useState<string|null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
 
-     const openModal = (image:string) => {
-        console.log("hey"+isModalOpen)
+  const openModal = (image: string) => {
     setSelectedImage(image);
     setIsModalOpen(true);
   };
@@ -22,32 +22,43 @@ export const ProductSection = () => {
 
   return (
     <div>
-      <h1 className="text-4xl font-bold mb-4 text-center mt-2 relative z-10 font-playfair">Explore Our Products</h1>
-            <section id="shop" className=" columns-1 lg:columns-3  p-1 relative z-10 ">
-              {ProductImageList.map((product) => (
-                <div key={product} className="  inline-block w-[100%] cursor-pointer "    onClick={() => openModal(product)} >
-                  <Image 
-                    src={`/Assets/${product}`} 
-                    alt={`Product ${product}`} 
-                    loading="lazy"
-                    width={0}
-                    height={0}
-                    sizes="100vw"
-                    className=" hover:opacity-80 transition-opacity w-full block  rounded-md     "
-                  />
-                </div>
-              ))}
+      <h1 className="text-4xl font-bold mb-4 text-center mt-2 relative z-10 font-playfair">
+        Explore Our Products
+      </h1>
+      <section
+        id="shop"
+        className=" columns-1 lg:columns-3  p-1 relative z-10 "
+      >
+        {ProductImageList.map((product) => (
+          <div
+            key={product}
+            className="  inline-block w-[100%] cursor-pointer "
+            onClick={() => openModal(product)}
+          >
+            <Image
+              src={`/Assets/${product}`}
+              alt={`Product ${product}`}
+              loading="lazy"
+              width={0}
+              height={0}
+              sizes="100vw"
+              className=" hover:opacity-80 transition-opacity w-full block  rounded-md     "
+            />
+          </div>
+        ))}
 
-
-<Modal isOpen={isModalOpen} onClose={closeModal}>
-     <div>
-        hey
-     </div>
-      </Modal>
-
-
-            </section>
-
+        <Modal isOpen={isModalOpen} onClose={closeModal}>
+          <Image
+            src={`/Assets/${selectedImage}`}
+            alt={`Product ${selectedImage}`}
+            loading="lazy"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className=" hover:opacity-80 transition-opacity w-full block  rounded-md     "
+          />
+        </Modal>
+      </section>
     </div>
-  )
-}
+  );
+};
